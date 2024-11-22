b2043a08a11a63030e5ddf92e92eba6c55626d8f
Marco Rampin <71671995+marcorampin@users.noreply.github.com>
2024-11-22 22:12:54 +0100

    Create ut-installer.sh

---
+++ ut-installer.sh
@@ -0,0 +1,4 @@
+# Unreal tournament files from Archive.org
+wget "https://archive.org/download/unreal-tournament-complete/Unreal%20Tournament.zip"
+# Patch 469d
+wget "https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469d/OldUnreal-UTPatch469d-Linux-amd64.tar.bz2"
