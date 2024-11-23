0fa6f2d50e0fe241dde4aca4e0b4e391125df353
Marco Rampin <rampin.marco.17@gmail.com>
2024-11-23 03:24:59 +0100

    Added check installed dependencies and progress messages

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # ut-linux-installer
 To do:
-- download files												V
-- unzip UT and patch files, then merge them in separate folder 	V
-- remove windows files (.dll & .exe) from System folder			V
-- remove compressed files										V
+- download files													\xE2\x9C\x94
+- unzip UT and patch files, then merge them in separate folder 		\xE2\x9C\x94
+- remove windows files (.dll & .exe) from System folder				\xE2\x9C\x94
+- remove compressed files											\xE2\x9C\x94
 
 Extra:
-- check if dependencies are installed
+- check if dependencies are installed								\xE2\x9C\x94
+- Add connection time limit
 - check system (amd64, arm64, x86) and download the correct patch
-- print statement for each started and completed step
+- print statement for each started and completed step				\xE2\x9C\x94
 - ask if .desktop and .appmenu links need to be created
+- ask if compressed folders need to be deleted
 - say which file needs to be executed to start the game
 
 Dependencies:
+coreutils
 wget
 unzip
 tar
--- ut-installer.sh
+++ ut-installer.sh
@@ -1,3 +1,30 @@
+echo 'Checking dependencies...'
+
+if pacman -Q coreutils &>/dev/null; then
+	echo -e '\xE2\x9C\x94 coreutils'
+else
+	echo 'coreutils missing'
+	exit 0
+fi
+if pacman -Q wget &>/dev/null; then
+	echo -e '\xE2\x9C\x94 wget'
+else
+	echo 'wget missing'
+	exit 0
+fi
+if pacman -Q unzip &>/dev/null; then
+	echo -e '\xE2\x9C\x94 unzip'
+else
+	echo 'unzip missing'
+	exit 0
+fi
+if pacman -Q tar &>/dev/null; then
+	echo -e '\xE2\x9C\x94 tar'
+else
+	echo 'tar missing'
+	exit 0
+fi
+
 FOLD_NAME='Unreal_tournament'
 ZIP_NAME='unreal_tournament.zip'
 TAR_NAME='patch.tar.bz2'
@@ -5,20 +32,34 @@ UT_ZIP=./$ZIP_NAME
 PATCH_TAR=./$FOLD_NAME/$TAR_NAME 
 
 # Unreal tournament files from Archive.org
+echo 'Downloading UT99 files...'
 wget -nv "https://archive.org/download/unreal-tournament-complete/Unreal%20Tournament.zip"
+echo -e '\xE2\x9C\x94 UT99 files downloaded'
 
+echo 'Extracting files...'
 mv ./'Unreal Tournament.zip' ./$ZIP_NAME
 unzip -q $UT_ZIP
 mv ./'Unreal Tournament' ./$FOLD_NAME
+echo -e '\xE2\x9C\x94 Files extracted'
 
 # Patch 469d
+echo 'Downloading 469d patch...'
 wget -P ./$FOLD_NAME -nv "https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469d/OldUnreal-UTPatch469d-Linux-amd64.tar.bz2"
+echo -e '\xE2\x9C\x94 Patch downloaded'
 
+echo 'Extracting and adding patch...'
 mv ./$FOLD_NAME/*.tar.bz2 ./$FOLD_NAME/$TAR_NAME
 tar -xf $PATCH_TAR -C ./$FOLD_NAME/ --overwrite
+echo -e '\xE2\x9C\x94 Patch added'
 
+echo 'Removing windows files...'
 rm ./$FOLD_NAME/System/*.dll
 rm ./$FOLD_NAME/System/*.exe
+echo -e '\xE2\x9C\x94 Windows files removed'
 
+echo 'Deleting compressed folders...'
 rm $UT_ZIP
 rm $PATCH_TAR
+echo -e '\xE2\x9C\x94 Compressed folders deleted'
+
+echo -e '\xE2\x9C\x94 Installation completed, execute '$FOLD_NAME'/System64/ut-bin-amd64 to play'
